a53aea6ebdc2065c9d60ccb109e900188c0ec633
tomaszjacek <tomasz_jacek@wp.pl>
2025-03-08 22:57:13 +0100

    dodany katalog projectR z wygenerowanym projektem rhino

---
+++ projectR/dependencies.R
@@ -0,0 +1,9 @@
+# This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
+library(rhino)
+library(treesitter)
+library(treesitter.r)
+library(reactable)
+library(echarts4r)
+library(dplyr)
+library(tidyr)
+library(htmlwidgets)
